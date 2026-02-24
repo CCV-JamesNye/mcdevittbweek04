5ab2d0111009ffaa9cc03a16e44b6fbf5e7378b7
mcdevittb <mcdevitt.brandon@gmail.com>
2026-02-24 00:10:23 -0400

    player collisions

--- Scenes/Player/Scripts/player-characterbody.gd
+++ Scenes/Player/Scripts/player-characterbody.gd
@@ -42,3 +42,7 @@ func _physics_process(delta: float) -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump") and is_on_floor():
 		velocity.y = jump_force
+		
+func die () -> void:
+	print ("player died")
+	get_tree().call_deferred('reload_current_scene')

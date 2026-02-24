c00b17e4f3e9e26b9cd65deedda42305abc3d073
mcdevittb <mcdevitt.brandon@gmail.com>
2026-02-23 23:28:56 -0400

    Initial commit

---
+++ Scenes/Player/Scripts/player.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var speed : float = 400
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _process(delta: float) -> void:
+	# Store Direction
+	var direction : Vector2 = Vector2.ZERO 
+
+	# Read Input
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+	
+	# Apply the Movement
+	position += direction.normalized() * speed * delta
+	pass
